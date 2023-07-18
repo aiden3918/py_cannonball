bbef96dd8767084018f8db940d03cae4d7af08b6
liaiden3918 <liaiden3918@gmail.com>
2023-07-17 23:19:34 -0700

    added user input (may change)

--- gui.py
+++ gui.py
@@ -1,11 +1,12 @@
 import pygame
+import math
 
 pygame.font.init()
 number_keys_input = [pygame.K_0, pygame.K_1, pygame.K_2, pygame.K_3, pygame.K_4, pygame.K_5, pygame.K_6, pygame.K_7, pygame.K_8, pygame.K_9]
 
 # create button classes to use in game
 class Button():
-    def __init__(self, x, y, image, scale): 
+    def __init__(self, x: int, y: int, image, scale: float) -> None: 
         width = image.get_width()
         height = image.get_height()
         self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
@@ -31,12 +32,12 @@ class Button():
 
 
 class Number_Input():
-    def __init__(self, x, y, font_type, font_size, r, g, b, min, max):
+    def __init__(self, x: int, y: int, font_type: str, font_size: int, color: tuple, min: float, max: float) -> None:
         self.element = pygame.font.SysFont(font_type, font_size)
         self.y = y
         self.x = x
         self.text = ''
-        self.rgb = (r, g, b)
+        self.rgb = color
         self.selected = False
         self.font_size = font_size
 
@@ -66,10 +67,38 @@ class Number_Input():
     # first try too im goated
     # now make it check that its selected using a selected/active var and give it min/max cap values
 
+class Slider():
+    # pos should be the center of the slider and where the button to drag begins (x, y)
+    # size is the length and width of the slider (width, height)
+    def __init__(self, pos: tuple, size: tuple, initial_val: float, min: int, max: int, container_color: tuple, slider_color: tuple, round_num_decimals: int) -> None:
+        self.pos = pos
+        self.size = size
+        self.slider_right_pos = self.pos[0] + (size[0] // 2)
+        self.slider_left_pos = self.pos[0] - (size[0] // 2)
+        self.slider_top_pos = self.pos[1] - (size[1] // 2)
 
-        
+        self.container_color = container_color
+        self.slider_color = slider_color
 
+        self.min = min
+        self.max = max
+        self.initial_val = (self.slider_right_pos - self.slider_left_pos) * initial_val # <- float (0 to 1)
 
-        
+        self.rounded_to_nearest_decimal = round_num_decimals
 
-        
+        self.container_rect = pygame.Rect(self.slider_left_pos, self.slider_top_pos, self.size[0], self.size[1])
+        self.drag_button_rect = pygame.Rect(self.slider_left_pos + self.initial_val - 5, self.slider_top_pos - 2, 10, self.size[1] + 4) # will stick out 2 pixels top and bottom
+
+    def update(self, window, mouse_pos, mouse_pressed): # this will likely conflict with display upldate, unless i call it at the very end, which is kind of awkward
+        if self.container_rect.collidepoint(mouse_pos) and mouse_pressed[0]:
+            self.drag_button_rect.centerx = mouse_pos[0]
+
+        pygame.draw.rect(window, self.container_color, self.container_rect)
+        pygame.draw.rect(window, self.slider_color, self.drag_button_rect)
+
+    def get_value(self):
+        val_range = self.slider_right_pos - self.slider_left_pos
+        current_val = self.drag_button_rect.centerx - self.slider_left_pos
+
+        rounding = math.pow(10, self.rounded_to_nearest_decimal)
+        return round(((current_val / val_range) * (self.max - self.min) + self.min) * rounding) / rounding
--- main.py
+++ main.py
@@ -20,6 +20,7 @@ WINDOW = pygame.display.set_mode(WINDOW_RES)
 pygame.display.set_caption("Cannonball - Python/Pygame")
 WINDOW_CENTER = (WINDOW_RES[0] // 2, WINDOW_RES[1] // 2)
 
+# background
 GRID_BG = pygame.image.load(os.path.join('Assets', 'grid-background-white.png'))
 GRID_BG = pygame.transform.scale(GRID_BG, WINDOW_RES)
 
@@ -33,14 +34,40 @@ CANNONBALL = pygame.transform.scale(CANNONBALL, (20, 20))
 
 BARREL_CENTER = (110, 610)
 
+# performance data
 FPS_TEXT = pygame.font.SysFont('arial', 22)
 SECONDS_SINCE_EVENT_TEXT = pygame.font.SysFont('arial', 22)
 FRAMES_SINCE_EVENT_TEXT = pygame.font.SysFont('arial', 22)
 ANGLE_TEXT = pygame.font.SysFont('arial', 22)
 
+# firing state variables
 seconds_since_event = 0
 frames_since_event = 0
 tracer_dots = []
+
+# user input panel
+user_input_panel = pygame.Surface((880, 200))
+user_input_panel.set_alpha(200)
+user_input_panel.fill((150, 150, 150))
+
+# user inputs and their labels
+WHITISH = (200, 200, 200)
+GRAYISH = (100, 100, 100)
+WHITE = (255, 255, 255)
+
+force_slider_input = gui.Slider((400, 50), (300, 20), 0.5, 0, 10000, WHITISH, GRAYISH, 2) # maybe make this a number input
+cannonball_mass_slider_input = gui.Slider((400, 120), (100, 20), 0.5, 0, 100, WHITISH, GRAYISH, 1)
+gravity_slider_input = gui.Slider((800, 50), (100, 20), 0.5, 0, 20, WHITISH, GRAYISH, 1)
+barrel_length_slider_input = gui.Slider((800, 120), (100, 20), 0.5, 0, 10, WHITISH, GRAYISH, 1)
+user_inputs = [force_slider_input, cannonball_mass_slider_input, gravity_slider_input, barrel_length_slider_input]
+
+force_text = pygame.font.SysFont('arial', 22)
+cannonball_mass_text = pygame.font.SysFont('arial', 22)
+gravity_text = pygame.font.SysFont('arial', 22)
+barrel_length_text = pygame.font.SysFont('arial', 22)
+
+# sounds -----------------------------------------------------------------------------------
+CANNON_SHOOT_SOUND = pygame.mixer.Sound(os.path.join('Assets', 'cannon-shoot.mp3'))
 
 # some constants and variables -----------------------------------------------------------------------------------
 FPS = 60
@@ -48,11 +75,12 @@ BACKGROUND_COLOR = (100, 100, 100)
 GREENISH = (105, 150, 62)
 pixels_to_meters_rate = 11.2 # 11.2 is the # of pixelsp for every simulated meter
 
-# update window -----------------------------------------------------------------------------------
+# update background -----------------------------------------------------------------------------------
 def update_background():
     WINDOW.fill(BACKGROUND_COLOR)
     WINDOW.blit(GRID_BG, (0, 0))
 
+# update cannon when aiming -----------------------------------------------------------------------------------
 def aiming_and_config_render(app_state, mouse, BARREL):
     ray_adj = mouse[0] - BARREL_CENTER[0]
     # switch coordinates to read that y-axis is at the bottom
@@ -70,6 +98,24 @@ def aiming_and_config_render(app_state, mouse, BARREL):
 
     return [barrel_copy, theta]
 
+# render user input elements during aiming and config -----------------------------------------------------------------------------------
+def render_inputs(mouse_pos, mouse_pressed):
+    WINDOW.blit(user_input_panel, (200, 0))
+
+    for input in user_inputs:
+        input.update(WINDOW, mouse_pos, mouse_pressed)
+    
+    force_text_rendered = force_text.render(f'Force: {force_slider_input.get_value()} N', 1, WHITE)
+    cb_mass_rendered = cannonball_mass_text.render(f'Cannonball Mass: {cannonball_mass_slider_input.get_value()} kg', 1, WHITE)
+    gravity_rendered = gravity_text.render(f'Gravity: {gravity_slider_input.get_value()} m/s^2', 1, WHITE)
+    barrel_length_rendered = barrel_length_text.render(f'Barrel Length: {barrel_length_slider_input.get_value()} m', 1, WHITE)
+
+    WINDOW.blit(force_text_rendered, (force_slider_input.pos[0] - (force_text_rendered.get_width() // 2), force_slider_input.pos[1] - 40))
+    WINDOW.blit(cb_mass_rendered, (cannonball_mass_slider_input.pos[0] - (cb_mass_rendered.get_width() // 2), cannonball_mass_slider_input.pos[1] - 40))
+    WINDOW.blit(gravity_rendered, (gravity_slider_input.pos[0] - (gravity_rendered.get_width() // 2), gravity_slider_input.pos[1] - 40))
+    WINDOW.blit(barrel_length_rendered, (barrel_length_slider_input.pos[0] - (barrel_length_rendered.get_width() // 2), barrel_length_slider_input.pos[1] - 40))
+
+# calculate forces and information needed when user fires -----------------------------------------------------------------------------------
 # return time it would take for the cannonball to exit the cannon and the net velocity at which it will exit the cannon
 # if there is not enough force, then returns list (-1, -1)
 def calc_prereq_forces(gravity, force, cb_mass, barrel_length, angle):
@@ -113,6 +159,7 @@ def calc_prereq_forces(gravity, force, cb_mass, barrel_length, angle):
 
         return (exit_cannon_time, exit_cannon_vel)
 
+# render motion of cannonball -----------------------------------------------------------------------------------
 def render_cannonball_motion(cannon_barrel_copy_list, hori_vel, vert_vel, cannon_opening_pos, gravity, frames_elapsed):
     # calculate displacement
     time = (frames_elapsed / FPS)
@@ -137,6 +184,7 @@ def render_cannonball_motion(cannon_barrel_copy_list, hori_vel, vert_vel, cannon
     for coordinate in tracer_dots:
         pygame.draw.circle(WINDOW, (200, 0, 0), coordinate, 5)
 
+# update performance data on the bottom -----------------------------------------------------------------------------------
 def update_display_data(clock, frames_elapsed, angle):
     # render data during launch (after cannonball leaves cannon)
     FPS_DISPLAY = FPS_TEXT.render(f'FPS : {int(clock.get_fps())}', 1, GREENISH)
@@ -146,13 +194,11 @@ def update_display_data(clock, frames_elapsed, angle):
     rounded_angle = round(angle * 100) / 100
     ANGLE_DISPLAY = ANGLE_TEXT.render(f"Angle: {rounded_angle}", 1, GREENISH)
 
-    WINDOW.blit(FPS_DISPLAY, (20, 20))
-    WINDOW.blit(SECONDS_SINCE_EVENT_DISPLAY, (20, 50))
-    WINDOW.blit(FRAMES_SINCE_EVENT_DISPLAY, (20, 80))
-    WINDOW.blit(ANGLE_DISPLAY, (300, 20))
+    WINDOW.blit(FPS_DISPLAY, (20, 660))
+    WINDOW.blit(SECONDS_SINCE_EVENT_DISPLAY, (20, 690))
+    WINDOW.blit(FRAMES_SINCE_EVENT_DISPLAY, (300, 660))
+    WINDOW.blit(ANGLE_DISPLAY, (300, 690))
 
-def render_inputs():
-    pass
 
 # main function -----------------------------------------------------------------------------------
 def main():
@@ -167,6 +213,7 @@ def main():
         clock.tick(FPS)
 
         mouse = pygame.mouse.get_pos()
+        mouse_pressed = pygame.mouse.get_pressed()
         keys_pressed = pygame.key.get_pressed()
 
         for event in pygame.event.get():
@@ -177,19 +224,21 @@ def main():
 
         if keys_pressed[pygame.K_SPACE]:
             if app_state == 'aiming and config':
-                cb_exit_time_and_vel = calc_prereq_forces(gravity=9.8, force=500, cb_mass=10, barrel_length=5, angle=barrel_angle) # test values
+                cb_exit_time_and_vel = calc_prereq_forces(gravity=gravity_slider_input.get_value(), force=force_slider_input.get_value(), cb_mass=cannonball_mass_slider_input.get_value(), barrel_length=5, angle=barrel_angle) # test values
                 if cb_exit_time_and_vel != (-1, -1):
                     app_state = 'firing'
 
         update_background()
 
+        # for different "states" of application (what is currently happening)
         match app_state:
             case "aiming and config":
                 frames_elapsed = 0
                 cannon_barrel_and_angle = aiming_and_config_render(app_state, mouse, BARREL)
                 barrel_angle = cannon_barrel_and_angle[1]
-                render_inputs()
+                render_inputs(mouse, mouse_pressed)
             case "firing":
+                CANNON_SHOOT_SOUND.play()
                 pygame.time.delay(int(cb_exit_time_and_vel[0] * 1000)) # simulate cannonball firing out of cannon by just waiting for it to come out lol 
                 # whichever velocity = cannonball's exit velocity * math.trig(angle of cannon)
                 hori_init_vel = cb_exit_time_and_vel[1] * math.cos(barrel_angle * math.pi / 180)
@@ -217,5 +266,4 @@ def main():
 # call it all babyyyyyy -----------------------------------------------------------------------------------
 if __name__ == "__main__":
     main()
-
 
